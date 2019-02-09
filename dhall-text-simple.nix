80e7693a94daf1e64751d2aac552aad747adef0e
justinwoo <moomoowoo@gmail.com>
2019-02-09 12:15:00 +0200

    update

--- dhall-text-simple.nix
+++ dhall-text-simple.nix
@@ -2,12 +2,11 @@
 
 pkgs.stdenv.mkDerivation rec {
   name = "dhall-text-simple";
-  version = "1.0.14";
-  dhall-version = "1.19.1";
+  version = "1.20.1";
 
   src = pkgs.fetchurl {
-    url = "https://github.com/dhall-lang/dhall-haskell/releases/download/${dhall-version}/dhall-text-${version}-x86_64-linux.tar.bz2";
-    sha256 = "1z3rxl9fmm9lqg8mlis36aybmvg3dhdz9pbgs1vs4dxk33ig6qmj";
+    url = "https://github.com/dhall-lang/dhall-haskell/releases/download/${version}/dhall-text-${version}-x86_64-linux.tar.bz2";
+    sha256 = "1jly3v6sd2bymfa51c57kq6vzkvz1hr69gvlwpjpqimxvkk09hs8";
   };
 
   installPhase = ''
