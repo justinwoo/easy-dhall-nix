7dc086decbef4bc9d81f8398eddb4056692fcff1
justinwoo <moomoowoo@gmail.com>
2019-08-03 23:37:49 +0200

    fix derivations missing usages of pkgs

--- default.nix
+++ default.nix
@@ -1,9 +1,9 @@
 { pkgs ? import <nixpkgs> {} }:
 
 {
-  dhall-simple = import ./dhall-simple.nix {};
+  dhall-simple = import ./dhall-simple.nix { inherit pkgs; };
 
-  dhall-json-simple = import ./dhall-json-simple.nix {};
+  dhall-json-simple = import ./dhall-json-simple.nix { inherit pkgs; };
 
-  dhall-bash-simple = import ./dhall-bash-simple.nix {};
+  dhall-bash-simple = import ./dhall-bash-simple.nix { inherit pkgs; };
 }
