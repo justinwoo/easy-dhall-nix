21e4726e7afef16599818f6a0d4f37fde6c765b4
Jack Henahan <jhenahan@me.com>
2019-08-07 10:06:11 -0400

    Add src for Darwin builds. Fixes #5.

--- dhall-bash-simple.nix
+++ dhall-bash-simple.nix
@@ -3,10 +3,15 @@
 pkgs.stdenv.mkDerivation rec {
   name = "dhall-bash-simple";
 
-  src = pkgs.fetchurl {
-    url = "https://github.com/dhall-lang/dhall-haskell/releases/download/1.25.0/dhall-bash-1.0.22-x86_64-linux.tar.bz2";
-    sha256 = "1849qacj6xk3ki8hyw3862jrabq2gp44sjay0r9iiygifzm51pk6";
-  };
+  src = if pkgs.stdenv.isDarwin
+    then pkgs.fetchurl {
+      url = "https://github.com/dhall-lang/dhall-haskell/releases/download/1.25.0/dhall-bash-1.0.22-x86_64-macos.tar.bz2";
+      sha256 = "1dqahd3rnsi8rh14cxyn7xvzg9p9brjqmymy085a1iq2bykqydp1";
+    }
+    else pkgs.fetchurl {
+      url = "https://github.com/dhall-lang/dhall-haskell/releases/download/1.25.0/dhall-bash-1.0.22-x86_64-linux.tar.bz2";
+      sha256 = "1849qacj6xk3ki8hyw3862jrabq2gp44sjay0r9iiygifzm51pk6";
+    };
 
   installPhase = ''
     mkdir -p $out/bin
