6db1fe885f354e3ca951dccd5dd68588758a7da4
Profpatsch <mail@profpatsch.de>
2020-11-30 22:53:27 +0100

    fix(fetch.py): when fetch is run, first update unstable channel
    This way we won’t forget to do it for months at a time.
    
    You’d hope that stuff like this would be possible without jumping
    through hoops, but this is nix after all. Flakes our salvation or
    something.

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,6 +1,6 @@
-import (
-  builtins.fetchTarball {
-    url = "https://github.com/nixos/nixpkgs/archive/a7bf8161fa834a602278c15fcbdd955656b3aed8.tar.gz";
-    sha256 = "0lzd35niw0j8qy4mhglvqwv5qwyvdb9mhv8hpmn73ym57r92i4p1";
-  }
-)
+import (fetchTarball {
+  inherit (builtins.fromJSON (builtins.readFile ./nixpkgs.json))
+    url
+    sha256
+    ;
+})
